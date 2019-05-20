d731f9c9b4bc6cd4181b8f5d83a4473eb1acb328
Rok <rok@mihevc.org>
2019-05-20 14:41:59 +0200

    Bugfix.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ URL = 'https://github.com/rok/label-wrapper'
 EMAIL = 'me@rok.dev'
 AUTHOR = 'Rok Mihevc'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
